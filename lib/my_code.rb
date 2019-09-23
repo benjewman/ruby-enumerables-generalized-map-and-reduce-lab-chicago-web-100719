7382dc3240b462e133315bb1f8d434b7f5298db5
Benjamin Davis <bennyadave@gmail.com>
2019-09-23 02:44:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  array.length.times do |index|
+    new_array[index] = yield(array[index])
+  end
+  return new_array
+end
+
+def reduce(array, tally = 0)
+  array.length.times do |index|
+    tally = yield(tally, array[index])
+  end
+  tally
+end
